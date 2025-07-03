059ae10e582839c911c957216b1eeaf9dda30783
fadli154 <fadlihifziansyah154@gmail.com>
2025-07-03 19:59:51 +0700

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import ContentSection from "@/components/content-1";
+import Features from "@/components/features-1";
+import FooterSection from "@/components/footer";
+import HeroSection from "@/components/hero-section";
+import TeamSection from "@/components/team";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Features />
+      <TeamSection />
+      <FooterSection />
+    </>
+  );
+}
